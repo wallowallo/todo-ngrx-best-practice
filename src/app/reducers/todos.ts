f93cbe8a6449bf1ac55936c316c7f975468d52b4
Krister <krister.torsvik@gmail.com>
2017-05-08 11:19:58 +0200

    fixed ternary spacing

--- src/app/reducers/todos.ts
+++ src/app/reducers/todos.ts
@@ -1,6 +1,5 @@
 const initialState = [];
 
-
 export function todos ( state = initialState, { type, payload } ) {
   switch ( type ) {
     case 'ADD_TODO':
@@ -10,24 +9,24 @@ export function todos ( state = initialState, { type, payload } ) {
       ];
 
     case 'UPDATE_TODO':
-      return state.map( todo => ( todo.id === payload.id ) ?
-        ({ ...todo, title: payload.title, isEdit: false, completed: false }) :
-        todo );
+      return state.map( todo => ( todo.id === payload.id )
+        ? ({ ...todo, title: payload.title, isEdit: false, completed: false })
+        : todo );
 
     case 'REMOVE_TODO':
       return state.filter( todo => todo.id !== payload );
 
     case 'TOGGLE_TODO_COMPLETED':
       return state.map( todo =>
-        ( todo.id === payload ) ?
-        ({ ...todo, completed: !todo.completed }) :
-        todo );
+        ( todo.id === payload )
+        ? ({ ...todo, completed: !todo.completed })
+        : todo );
 
     case 'TOGGLE_EDIT_TODO':
       return state.map( todo =>
-        ( todo.id === payload ) ?
-        ({ ...todo, isEdit: !todo.isEdit }) :
-        ({ ...todo, isEdit: todo.isEdit = false }) );
+        ( todo.id === payload )
+        ? ({ ...todo, isEdit: !todo.isEdit })
+        : ({ ...todo, isEdit: todo.isEdit = false }) );
 
     default:
       return state;
